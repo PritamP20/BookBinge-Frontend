1ac960a35ff7c44b7408127abf7bbd81dfe2e104
PritamP20 <pripritam7@gmail.com>
2024-04-05 21:30:24 +0530

    mistake

--- src/components/DocumentView.jsx
+++ src/components/DocumentView.jsx
@@ -1,61 +1,30 @@
-// import React from 'react'
-// import { useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import { Document, Page } from 'react-pdf';
-
-// const DocumentView = () => {
-
-//     const location = useLocation();
-//     const { ClgNotes } = location.state;
-//     const ClgNotesUrl = ClgNotes.ClgNotes
-//     console.log("clgurl: "+ClgNotesUrl)
-
-//     const [numPages, setNumPages] = useState(null);
-//     const [pageNumber, setPageNumber] = useState(1);
-
-//     function onDocumentLoadSuccess({ numPages }) {
-//       setNumPages(numPages);
-//     }
-
-//   return (
-//     <div className='d-flex justify-content-center align-items-center'>
-//       {/* <embed className='d-flex justify-content-center align-items-center' src={ClgNotesUrl} type="application/pdf" width="100%" height="900px" /> */}
-//       <iframe className="w-100 d-flex justify-content-center align-items-center" height="900p" src={ClgNotesUrl} frameborder="0"></iframe>
-//     </div>
-//   )
-// }
-
-// export default DocumentView
-
-import React, { useState } from 'react';
+import React from 'react'
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Document, Page } from 'react-pdf';
 
 const DocumentView = () => {
+
     const location = useLocation();
     const { ClgNotes } = location.state;
-    const ClgNotesUrl = ClgNotes.ClgNotes;
+    const ClgNotesUrl = ClgNotes.ClgNotes
+    console.log("clgurl: "+ClgNotesUrl)
 
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
 
     function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
+      setNumPages(numPages);
     }
 
-    return (
-        <div className='d-flex justify-content-center align-items-center'>
-            <Document
-                file={ClgNotesUrl}
-                onLoadSuccess={onDocumentLoadSuccess}
-            >
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>Page {pageNumber} of {numPages}</p>
-        </div>
-    );
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+      {/* <embed className='d-flex justify-content-center align-items-center' src={ClgNotesUrl} type="application/pdf" width="100%" height="900px" /> */}
+      <iframe className="w-100 d-flex justify-content-center align-items-center" height="900p" src={ClgNotesUrl} frameborder="0"></iframe>
+    </div>
+  )
 }
 
-export default DocumentView;
+export default DocumentView
 
 
