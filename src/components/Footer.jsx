ff209c37f67eab6dc97c014c5f8f5a086f59b984
PritamP20 <pripritam7@gmail.com>
2024-04-06 06:53:49 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,25 +4,27 @@ import { FaFacebook, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='' style={{backgroundColor:"#ffedd5", fontFamily:'serif'}}>
-      <div className='container'>
-          <div className='p-lg-5'>
-            <div className='fs-2'>BookBinge</div>
+    <div className='' style={{backgroundColor:"black", fontFamily:'serif'}}>
+      <div className='container mt-5'>
+          <div className='p-lg-3'>
+            <div className='fs-3 d-flex text-white justify-content-center'>BookBinge</div>
             <div>
-              <span className='fs-4'>we have create this website for your benefit so make the full use of it</span>
+              <span className='fs-5 d-flex text-white justify-content-center'>Facing any issue? Contact: 7972121969</span>
             </div>
-            <div className='fs-2 mt-lg-3 mt-md-3 mt-sm-3 mt-3'>Any Feedback:</div>
-            <div className='col-lg-5 col-md-8 col-sm-12 col-12'>
-            <div className="input-group ">
-                  <input type="text" className="form-control " placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                  <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            <div className='d-flex text-white justify-content-center fs-3 mt-lg-3 mt-md-3 mt-sm-3 mt-3'>Any Feedback:</div>
+            <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
+              <div className='d-flex justify-content-center m-auto  w-75'>
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                  <button class="btn text-white border border-2 border-white" type="button" id="button-addon2">Button</button>
                 </div>
+              </div>
             </div>
-          <div className='col-lg-6 d-flex gap-lg-5 gap-5 mt-lg-3 mt-3 justify-content-center'>
-            <a href='' className='text-decoration-none text-black fs-1'><FaFacebook/></a>
-            <a href='' className='text-decoration-none text-black fs-1'><FaInstagram/></a>
-            <a href='' className='text-decoration-none text-black fs-1'><FaGithub/></a>
-            <a href='' className='text-decoration-none text-black fs-1'><FaLinkedin/></a>
+          <div className='col-lg-12 d-flex gap-lg-5 gap-5 mt-lg-3 mt-3 justify-content-center'>
+            <a href='' className='text-decoration-none text-white fs-3'><FaFacebook/></a>
+            <a href='' className='text-decoration-none text-white fs-3'><FaInstagram/></a>
+            <a href='' className='text-decoration-none text-white fs-3'><FaGithub/></a>
+            <a href='' className='text-decoration-none text-white fs-3'><FaLinkedin/></a>
           </div>
         </div>
       </div>
