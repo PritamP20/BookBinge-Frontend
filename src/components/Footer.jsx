93ccea053545e8366df4669cf23ac4c2dc849cad
PritamP20 <pripritam7@gmail.com>
2024-04-06 07:02:53 +0530

    again footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,18 @@ const Footer = () => {
           <div className='p-lg-3'>
             <div className='fs-3 d-flex text-white justify-content-center'>BookBinge</div>
             <div>
-              <span className='fs-5 d-flex text-white justify-content-center'>Facing any issue? Contact: 7972121969</span>
+              <span className='fs-6 d-flex text-white justify-content-center'>Facing any issue? Contact: 7972121969</span>
             </div>
-            <div className='d-flex text-white justify-content-center fs-3 mt-lg-3 mt-md-3 mt-sm-3 mt-3'>Any Feedback:</div>
+            <div className='d-flex text-white justify-content-center fs-4 mt-1'>Any Feedback:</div>
             <div className='col-lg-12 col-md-12 col-sm-12 col-12'>
-              <div className='d-flex justify-content-center m-auto  w-75'>
+              <div className='d-flex justify-content-center m-auto '>
                 <div className="input-group">
                   <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                   <button class="btn text-white border border-2 border-white" type="button" id="button-addon2">Button</button>
                 </div>
               </div>
             </div>
-          <div className='col-lg-12 d-flex gap-lg-5 gap-5 mt-lg-3 mt-3 justify-content-center'>
+          <div className='col-lg-12 d-flex gap-lg-5 gap-3 mt-lg-3 mt-3 justify-content-center'>
             <a href='' className='text-decoration-none text-white fs-3'><FaFacebook/></a>
             <a href='' className='text-decoration-none text-white fs-3'><FaInstagram/></a>
             <a href='' className='text-decoration-none text-white fs-3'><FaGithub/></a>
